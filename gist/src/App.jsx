4958f95558c6baf7bbd720f1955d8a19a5b71475
unknown <sainikith0301@gmail.com>
2024-07-02 23:17:27 +0530

    error mapping in frontend

--- gist/src/App.jsx
+++ gist/src/App.jsx
@@ -1,27 +1,40 @@
-import { useEffect, useState } from 'react'
-import { useDispatch,useSelector } from "react-redux";
-import './App.css'
-import TextEditor from './frontend/components/TextEditor'
-import TextArea from './frontend/components/TextArea'
-import SearchBar from './frontend/components/SearchBar';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import TextEditor from "./frontend/components/TextEditor";
+import TextArea from "./frontend/components/TextArea";
+import SearchBar from "./frontend/components/SearchBar";
+import NavBar from "./frontend/components/NavBar";
+import SignUp from "./frontend/pages/SignUp";
+import Login from "./frontend/pages/Login";
 
 function App() {
+  const edit = useSelector((state) => state.flag.value);
 
-const edit=useSelector((state)=>state.flag.value)
-
-useEffect(()=>{console.log("App edit state-",edit)},[edit])
+  useEffect(() => {
+    console.log("App edit state-", edit);
+  }, [edit]);
   // const [edit, setEdit] = useState(0)
 
   return (
     <>
-<SearchBar/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<SearchBar />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <SearchBar/>
 <p>. </p>
 <p>. </p>
-<p>. </p>
-    {edit? <TextArea/>  :   <TextEditor/>}
-  
+
+    {edit? <TextArea/>  :   <TextEditor/>} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ gist/src/frontend/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function NavBar() {
+  return (
+    <>
+    
+
+<nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+        <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <a href="tel:5541251234" className="text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
+            <Link to="/login" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</Link>
+            <Link to="/signup" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Sign Up</Link>
+        </div>
+    </div>
+</nav>
+<nav className="bg-gray-50 dark:bg-gray-700">
+    <div className="max-w-screen-xl px-4 py-3 mx-auto">
+        <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                <li>
+                    <Link to="/" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</Link>
+                </li>
+              
+                <li>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Features</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+    </>
+  )
+}
